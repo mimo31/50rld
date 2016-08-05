4bab3b0f1f605d5b02728f0321ae01aa6fc6f147
mimo31 <weweuuuu@email.cz>
2016-08-05 21:17:33 +0200

    Drawing the player (well, simply), responding to keys with movement and zooming and a little bit of balancing done also

--- Core/50rld/src/com/github/mimo31/w50rld/Constants.java
+++ Core/50rld/src/com/github/mimo31/w50rld/Constants.java
@@ -8,7 +8,7 @@ package com.github.mimo31.w50rld;
 public class Constants {
 
 	// scale for the biome generation, the bigger the scale, the bigger the biomes
-	public static final int BIOME_SCALE = 32;
+	public static final int BIOME_SCALE = 64;
 	
 	// scale for the ore generation, the bigger the scale, the bigger the ore deposits and the bigger spaces between them
 	public static final int ORE_SCALE = 32;
@@ -19,6 +19,12 @@ public class Constants {
 	public static final double GOLD_ORE_LIMIT = 0.7;
 	
 	// maximal possible zoom
-	public static final int MAX_ZOOM = 4;
+	public static final int MAX_ZOOM = 5;
+	
+	// number of milliseconds that will pass between the user presses an arrow key and the player starts continuously moving
+	public static final int MOVE_START_DELAY = 500;
+	
+	// minimal number of milliseconds that passes between the movement when the player is continuously moving
+	public static final int MOVE_INTERVAL = 25;
 	
 }
--- Core/50rld/src/com/github/mimo31/w50rld/Main.java
+++ Core/50rld/src/com/github/mimo31/w50rld/Main.java
@@ -1,10 +1,13 @@
 package com.github.mimo31.w50rld;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,11 +29,19 @@ public class Main {
 	// game seed
 	public static final long SEED = (long) (Math.random() * Long.MAX_VALUE);
 	
-	// the zoom of the map (size of one Tile is width * 2 ^ (8 - zoom))
-	public static int zoom = 0;
+	// the zoom of the map (size of one Tile is width * 2 ^ (9 - zoom))
+	public static int zoom = 2;
 	
 	// the map
 	public static Map map = new Map();
+	
+	// the arrow keycode that is currently pressed and we are therefore moving that direction (0 in no key is pressed)
+	private static int arrowDown = 0;
+	
+	// time when the arrow key was pressed
+	private static long downAt = 0;
+	// time when the last move was done
+	private static long lastMove = 0;
 	
 	public static void main(String[] args)
 	{
@@ -84,6 +95,25 @@ public class Main {
 			
 		});
 		
+		// add key listener
+		frame.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				Main.keyReleased(e);
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				Main.keyPressed(e);
+			}
+		});
+		
 		// start the update timer
 		updateTimer.start();
 		
@@ -100,7 +130,7 @@ public class Main {
 	public static void paint(Graphics2D g, int width, int height)
 	{
 		// the size of a Tile on the screen
-		float tileSize = (float) (width * Math.pow(2, zoom - 8));
+		float tileSize = (float) (width * Math.pow(2, zoom - 9));
 		
 		// coordinates of the rectangle of the Map that can be seen on the screen
 		float mapViewWidth = width * 7 / 8f / tileSize;
@@ -131,7 +161,92 @@ public class Main {
 				int nextPaintY = (int)((i + 1 - mapViewCornerY) * tileSize);
 				
 				currentTile.paint(g, paintX, paintY, nextPaintX - paintX, nextPaintY - paintY);
+
+				// draw the player
+				if (j == playerX && i == playerY)
+				{
+					g.setColor(Color.black);
+					int playerSquareX = (paintX + nextPaintX) / 2;
+					int playerSquareY = (paintY + nextPaintY) / 2;
+					g.fillRect(playerSquareX, playerSquareY, nextPaintX - playerSquareX, nextPaintY - playerSquareY);
+				}
 			}
+		}
+		
+		// draw the player
+		
+	}
+	
+	/**
+	 * Handles key releases for the game.
+	 * @param e KeyEvent
+	 */
+	public static void keyReleased(KeyEvent e)
+	{
+		int code = e.getKeyCode();
+		switch (code)
+		{
+			// handle zooming in and out
+			case KeyEvent.VK_ADD:
+			case KeyEvent.VK_PLUS:
+				if (zoom != Constants.MAX_ZOOM)
+				{
+					zoom++;
+				}
+				break;
+			case KeyEvent.VK_SUBTRACT:
+			case KeyEvent.VK_MINUS:
+				if (zoom != 0)
+				{
+					zoom--;
+				}
+				break;
+			
+			// handle arrow releases - stop moving
+			case KeyEvent.VK_UP:
+			case KeyEvent.VK_DOWN:
+			case KeyEvent.VK_LEFT:
+			case KeyEvent.VK_RIGHT:
+				arrowDown = (code == arrowDown) ? 0 : arrowDown;
+				break;
+		}
+	}
+	
+	/**
+	 * Handles key types for the game.
+	 * @param e KeyEvent
+	 */
+	public static void keyPressed(KeyEvent e)
+	{
+		int code = e.getKeyCode();
+		if (arrowDown == 0 && (code == KeyEvent.VK_UP || code == KeyEvent.VK_DOWN || code == KeyEvent.VK_LEFT || code == KeyEvent.VK_RIGHT))
+		{
+			arrowDown = code;
+			downAt = System.currentTimeMillis();
+			lastMove = 0;
+			movePlayer();
+		}
+	}
+	
+	/**
+	 * Changes the player's coordinates according to the arrow key that is pressed.
+	 */
+	private static void movePlayer()
+	{
+		switch (arrowDown)
+		{
+			case KeyEvent.VK_UP:
+				playerY--;
+				break;
+			case KeyEvent.VK_DOWN:
+				playerY++;
+				break;
+			case KeyEvent.VK_LEFT:
+				playerX--;
+				break;
+			case KeyEvent.VK_RIGHT:
+				playerX++;
+				break;
 		}
 	}
 	
@@ -143,8 +258,10 @@ public class Main {
 	 */
 	public static void update(int width, int height, int delta)
 	{
+		long currentTime = System.currentTimeMillis();
+		
 		// the size of a Tile on the screen
-		float tileSize = (float) (width * Math.pow(2, zoom - 8));
+		float tileSize = (float) (width * Math.pow(2, zoom - 9));
 		
 		// calculate the coordinates of the rectangle of the Map that can be seen on the screen
 		float mapViewWidth = width * 7 / 8f / tileSize;
@@ -154,6 +271,13 @@ public class Main {
 		
 		// prepare the Map Tiles in this rectangle
 		map.prepareTiles((int)Math.floor(mapViewCornerX), (int)Math.floor(mapViewCornerY), (int)Math.ceil(mapViewWidth), (int)Math.ceil(mapViewHeight));
+		
+		// move the player if needed
+		if (arrowDown != 0 && downAt + Constants.MOVE_START_DELAY <= currentTime && lastMove + Constants.MOVE_INTERVAL <= currentTime)
+		{
+			movePlayer();
+			lastMove = currentTime;
+		}
 	}
 	
 	/**
